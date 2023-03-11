062a62db49987262710dc133941770a9c4f05f85
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-03-11 12:19:03 +0300

    Fix Warp docker import

--- docker/nvidia/qwantify.sh
+++ docker/nvidia/qwantify.sh
@@ -2,7 +2,7 @@
  #Add VirtualGL directories to path
 export PATH="${PATH}:/opt/VirtualGL/bin"
 
-# Use VirtualGL to run wine with OpenGL if the GPU is available, otherwise use barebone wine
+# Use VirtualGL to run wine with OpenGL if the GPU is available, otherwise run vanilla wine
 if [ -n "$(nvidia-smi --query-gpu=uuid --format=csv | sed -n 2p)" ]; then
   export VGL_DISPLAY="${VGL_DISPLAY:-egl}"
   export VGL_REFRESHRATE="$REFRESH"
