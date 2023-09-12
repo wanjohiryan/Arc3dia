d8825aeaba62332d236722d2c7f2392ea14b631d
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-09-12 17:37:34 +0300

    Add documentation

---
+++ docs/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx'
+})
+
+module.exports = withNextra()
