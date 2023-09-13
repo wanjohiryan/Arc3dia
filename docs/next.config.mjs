99d8c23c0cbfb363421bfd6de39305c9911deb36
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-09-13 10:24:45 +0300

    TODO:

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,5 +1,5 @@
 import nextra from "nextra";
-
+{/**TODO: add app route */}
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -42,14 +42,14 @@ const logo = (
 
 const config: DocsThemeConfig = {
   project: {
-    link: 'https://github.com/shuding/nextra'
+    link: 'https://github.com/wanjohiryan/arc3dia'
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra/tree/main/docs',
+  docsRepositoryBase: 'https://github.com/wanjohiryan/arc3dia/tree/main/docs',
   useNextSeoProps() {
     const { asPath } = useRouter()
     if (asPath !== '/') {
       return {
-        titleTemplate: '%s – Nextra'
+        titleTemplate: '%s – Arc3dia'
       }
     }
   },
@@ -70,7 +70,7 @@ const config: DocsThemeConfig = {
         <meta httpEquiv="Content-Language" content="en" />
         <meta
           name="description"
-          content="Make beautiful websites with Next.js & MDX."
+          content=""
         />
         <meta
           name="og:description"
@@ -78,28 +78,16 @@ const config: DocsThemeConfig = {
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={socialCard} />
-        <meta name="twitter:site:domain" content="nextra.site" />
-        <meta name="twitter:url" content="https://nextra.site" />
+        <meta name="twitter:site:domain" content="arc3dia.com" />
+        <meta name="twitter:url" content="https://dev.arc3dia.com" />
         <meta
           name="og:title"
-          content={title ? title + ' – Nextra' : 'Nextra'}
+          content={title ? title + ' – Arc3dia' : 'Arc3dia'}
         />
         <meta name="og:image" content={socialCard} />
-        <meta name="apple-mobile-web-app-title" content="Nextra" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="icon" href="/favicon.png" type="image/png" />
-        <link
-          rel="icon"
-          href="/favicon-dark.svg"
-          type="image/svg+xml"
-          media="(prefers-color-scheme: dark)"
-        />
-        <link
-          rel="icon"
-          href="/favicon-dark.png"
-          type="image/png"
-          media="(prefers-color-scheme: dark)"
-        />
+        <meta name="apple-mobile-web-app-title" content="Arc3dia" />
+        {/**TODO:Fix this */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </>
     )
   },
@@ -109,6 +97,13 @@ const config: DocsThemeConfig = {
   feedback: {
     content: 'Question? Give us feedback →',
     labels: 'feedback'
+  },
+  themeSwitch: {
+    component() {
+      return (
+        <div></div>
+      )
+    }
   },
   sidebar: {
     titleComponent({ title, type }) {
@@ -123,27 +118,7 @@ const config: DocsThemeConfig = {
   footer: {
     text: (
       <div className="flex w-full flex-col items-center sm:items-start">
-        <div>
-          <a
-            className="flex items-center gap-1 text-current"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="vercel.com homepage"
-            href="https://vercel.com?utm_source=nextra.site"
-          >
-            <span>Powered by</span>
-            <svg height={20} viewBox="0 0 283 64" fill="none">
-              <title>Vercel</title>
-              <path
-                fill="currentColor"
-                d="M141.04 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.46 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM248.72 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.45 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM200.24 34c0 6 3.92 10 10 10 4.12 0 7.21-1.87 8.8-4.92l7.68 4.43c-3.18 5.3-9.14 8.49-16.48 8.49-11.05 0-19-7.2-19-18s7.96-18 19-18c7.34 0 13.29 3.19 16.48 8.49l-7.68 4.43c-1.59-3.05-4.68-4.92-8.8-4.92-6.07 0-10 4-10 10zm82.48-29v46h-9V5h9zM36.95 0L73.9 64H0L36.95 0zm92.38 5l-27.71 48L73.91 5H84.3l17.32 30 17.32-30h10.39zm58.91 12v9.69c-1-.29-2.06-.49-3.2-.49-5.81 0-10 4-10 10V51h-9V17h9v9.2c0-5.08 5.91-9.2 13.2-9.2z"
-              />
-            </svg>
-          </a>
-        </div>
-        <p className="mt-6 text-xs">
-          © {new Date().getFullYear()} The Nextra Project.
-        </p>
+
       </div>
     )
   },
