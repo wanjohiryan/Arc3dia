ac49d0959b49efe2fda7b4bf7113b7d15268051f
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-09-27 08:47:43 +0300

    Add Xserver and Pulseaudio (#96)
    * Add Xserver and Pulseaudio
    
    * Add temp folder
    
    * Add warp-client to ´temp´ folder
    
    * Add warp binary
    
    * Add xserver and pulseaudio
    
    * Fix xserver installation
    
    * add sysbox installation script
    
    * Added dbus xvfb and pulseaudio services
    
    * Fix symlinking
    
    * Fix dbus, xserver and pulseudio startup errors
    
    * Run as user
    
    * Fix build errors for user 'admin'
    
    * test ffmpeg
    
    * Add ffmpeg v6 manually
    
    * Fix ffmpeg version
    
    * Fix ffmpeg
    
    * Fix pulseaudio
    
    * Fix default.pa
    
    * Add non-root user
    
    * Add user-group docker
    
    * Start everything in its own service
    
    * fix xvfb.service
    
    * Fix ffmpeg.sh not running, stuck on Xserver socket
    
    * Fix No.2 for ffmpeg
    
    * Add `ExecStartPre`, might work
    
    * debug
    
    * debug 2
    
    * debug 3
    
    * debug 3
    
    * ffmpeg fix
    
    * Added a fix to sudo
    
    * debug 5
    
    * Remove the dbus service
    
    * Add a FIXME for future me

---
+++ docker/base/ffmpeg.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#
+#FIXME: Remove this file (it is here for refence) then fix this ffmpeg error ``Sep 27 05:27:13 2f07ec1815e1 arc3dia-server[1140]: [mp4 @ 0x56057dc86140] Application provided duration: -1 / timestamp: 2744278 is out of range for mov/mp4 format``
+# i think it is the ``streaming 1`` option.
+#
+
+CMD=(
+    ffmpeg
+    -hide_banner
+    -loglevel error
+    #screen image size
+    -s 1920x1080
+    #video fps
+    -r 30
+    #grab x11 display
+    -f x11grab
+        -i ${DISPLAY}
+    #capture pulse audio
+    -f pulse
+        -re
+        -i default
+    -c:v libx264 
+        -preset veryfast
+        -tune zerolatency
+        -profile main
+        -pix_fmt yuv420p #let us use 4:2:0 for now 
+        #FIXME: add full color 4:4:4
+    -c:a aac
+        -b:a 128k
+        -ar 44100
+        -ac 2
+    -map v:0 -s:v:0 1280x720 -b:v:0 3M
+    #FIXME: add this later
+    #-map v:0 -s:v:1 854x480  -b:v:1 1.1M
+    #-map v:0 -s:v:2 640x360  -b:v:2 365k
+    -map 1:a
+    -streaming 1
+    -movflags empty_moov+frag_every_frame+separate_moof+omit_tfhd_offset
+
+    ./playlist.mpd
+    #Then send this to our remote/local warp-relay server
+    #- | RUST_LOG=warp=info /usr/bin/warp -i - -u $WARP_SERVER_FULL_URL
+)
+
+exec "${CMD[@]}"
