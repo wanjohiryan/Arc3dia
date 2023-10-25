c41ebe1b57e5a40bddc191b76fa551d0e30fbbff
Wanjohi <71614375+wanjohiryan@users.noreply.github.com>
2023-10-25 11:56:54 +0300

    Integrate the warp client into ffmpeg (#99)
    * Add integration warp client
    
    * i don't know
    
    * Fix: Remove unnecessary dbus installations
    
    * fix systemd service file
    
    * Fix Env variables
    
    * Add sample video
    
    * Add some video file for testing purposes
    
    * Dowload warp
    
    * Install Jq and fix download errors
    
    * Clean up the comments

--- docker/base/entrypoint.sh
+++ docker/base/entrypoint.sh
@@ -4,6 +4,16 @@
 
 export DISPLAY="${DISPLAY:-:0}"
 
+WARP_SERVER_HOST="${WARP_HOST:-"localhost"}"
+WARP_SERVER_PORT="${WARP_PORT:-4443}"
+WARP_ADDRESS="${WARP_ADDRESS:-$WARP_SERVER_HOST:$WARP_SERVER_PORT}"
+
+# Generate a random 16 character name by default. for each container
+URL_NAME="${NAME:-$(head /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | head -c 16)}"
+
+#Full server url
+export WARP_FULL_URL="${URL:-"https://$WARP_ADDRESS/$URL_NAME"}"
+
 # Configure joystick interposer
 sudo mkdir -pm755 /dev/input
 sudo touch /dev/input/{js0,js1,js2,js3}
